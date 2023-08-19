b98992d97d002db1afc4683dcb8561fca371b83e
TetianaKabanova <110104725+TetianaKabanova@users.noreply.github.com>
2023-08-19 12:19:13 +0200

    Update ContactsPage.jsx

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -3,24 +3,21 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectAuthentificated } from 'redux/authReducer';
 import { requestContactsThunk } from 'redux/contactsOperations';
 import { Container } from './ContactsPage.styled';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { Section } from 'components/Section/Section';
 import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
-import {
-  selectContactsError,
-  selectContactsIsLoading,
-} from 'redux/contactsReducer';
+import { selectContactsIsLoading } from 'redux/contactsReducer';
 import { Spin } from 'antd';
-import { notifyOptions } from 'components/Notification/notifyOptions';
-import { errorMessage } from 'components/Notification/Notification';
+// import { notifyOptions } from 'components/Notification/notifyOptions';
+// import { errorMessage } from 'components/Notification/Notification';
 
 const ContactsPage = () => {
   const authentificated = useSelector(selectAuthentificated);
   const isLoading = useSelector(selectContactsIsLoading);
-  const error = useSelector(selectContactsError);
+  // const error = useSelector(selectContactsError);
 
   const dispatch = useDispatch();
 
@@ -38,7 +35,7 @@ const ContactsPage = () => {
       <ToastContainer />
       <Section title="Contacts">
         {isLoading && <Spin />}
-        {error && toast.error(`${errorMessage}`, notifyOptions)}
+        {/* {error && toast.error(`${errorMessage}`, notifyOptions)} */}
         <Filter title="Find contact by name" />
         <ContactList />
       </Section>
